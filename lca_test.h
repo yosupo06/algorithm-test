1049becbc1f206b67428d1f8e10efeef7733b251
Kohei Morita <yosupo06@gmail.com>
2018-09-30 22:48:08 +0900

    fix lca_test

--- lca_test.h
+++ lca_test.h
@@ -1,32 +1,71 @@
-#include "gtest/gtest.h"
-#include "random.h"
+#pragma once
 
+#include "gtest/gtest.h"
+#include "lca.h" // "正しい"LCAが入っている
+#include "random.h" // 乱数生成ライブラリ
+
+// ここから
 template<typename LCA>
 class LCATest : public ::testing::Test {
-    LCATest() {
-        struct E {
-            int to;
-        };
-        auto check = [&](vector <vector<E>> g) {
-            int n = int(g.size());
-            auto lca = get_lca(g, 0);
-            auto hl = get_hl(g, 0);
+};
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    ASSERT_EQ(lca.query(i, j), hl.lca(i, j));
-                }
-            }
-        };
+using testing::Types;
 
-        for (int ph = 0; ph < 100; ph++) {
-            VV <E> g(20);
-            for (int i = 1; i < 20; i++) {
-                int p = rand_int(0, i - 1);
-                g[i].push_back(E{p});
-                g[p].push_back(E{i});
+TYPED_TEST_CASE_P(LCATest);
+// ここまでおまじない
+
+// テストケース1: 小さなケースでのランダムテスト
+TYPED_TEST_P(LCATest, StressTest) {
+    struct E {
+        int to;
+    };
+    auto check = [&](std::vector <std::vector<E>> g) {
+        int n = int(g.size());
+        TypeParam your_lca;
+        your_lca.setup(g, 0);
+        auto my_lca = YosupoLCA::get_lca(g, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ASSERT_EQ(your_lca.query(i, j), my_lca.query(i, j));
             }
-            check(g);
         }
+    };
+
+    for (int ph = 0; ph < 100; ph++) {
+        std::vector <std::vector<E>> g(20);
+        for (int i = 1; i < 20; i++) {
+            int p = Random::rand_int(0, i - 1);
+            g[i].push_back(E{p});
+            g[p].push_back(E{i});
+        }
+        check(g);
     }
 }
+
+// テストケース2: 有向木を入れたときでも動くのが望ましい
+TYPED_TEST_P(LCATest, DirectedTest) {
+    struct E {
+        int to;
+    };
+    /*
+     * 2 - 1 - 0
+     *       \ 3
+     */
+    std::vector <std::vector<E>> g(4);
+    g[2].push_back(E{1});
+    g[1].push_back(E{0});
+    g[1].push_back(E{3});
+
+    TypeParam your_lca;
+    your_lca.setup(g, 2);
+
+    ASSERT_EQ(your_lca.query(2, 0), 2);
+    ASSERT_EQ(your_lca.query(3, 1), 1);
+    ASSERT_EQ(your_lca.query(0, 3), 1);
+    ASSERT_EQ(your_lca.query(3, 3), 3);
+}
+
+// おまじない…
+REGISTER_TYPED_TEST_CASE_P(LCATest,
+        StressTest, DirectedTest);
--- random.h
+++ random.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <random>
+
 namespace Random {
+    using ll = long long;
+    using namespace std;
     ll rand_int(ll l, ll r) { //[l, r]
         using D = uniform_int_distribution<ll>;
         static random_device rd;
