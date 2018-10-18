d710568826c51377345e7f85de15086d9ef5a911
Kohei Morita <yosupo06@gmail.com>
2018-10-18 17:59:07 +0900

    reformat

--- algotest/string/ahocorasick_test.h
+++ algotest/string/ahocorasick_test.h
@@ -11,9 +11,12 @@ class AhoCorasickTesterBase {
     /**
      * 長さ|patterns|の vector<vector<int>>を返す
      * それぞれの配列には、対応するpatternがtargetの中で出現する位置(の終端idx)を列挙する
-     * 例えばtarget = "abcabd", pattern = "ba"なら、{0(="ab...."), 3(="..ab..")}を返す
+     * 例えばtarget = "abcabd", pattern = "ba"なら、
+     * {0(="ab...."), 3(="..ab..")}を返す
      */
-    virtual std::vector<std::vector<int>> enumerate(std::string target, std::vector<std::string> patterns) = 0;
+    virtual std::vector<std::vector<int>> enumerate(
+        std::string target,
+        std::vector<std::string> patterns) = 0;
 };
 
 }  // namespace algotest
@@ -42,7 +45,7 @@ TYPED_TEST_P(AhoCorasickTest, StressTest) {
                 patterns[j] = gen.lower_string(10);
             } else {
                 int di = gen.uniform(1, n);
-                int st = gen.uniform(0, n-di);
+                int st = gen.uniform(0, n - di);
                 patterns[j] = target.substr(st, di);
             }
         }
@@ -52,7 +55,8 @@ TYPED_TEST_P(AhoCorasickTest, StressTest) {
             std::vector<int> v;
             int di = pattern.size();
             for (int s = 0; s + di <= n; s++) {
-                if (target.substr(s, di) == pattern) v.push_back(s);
+                if (target.substr(s, di) == pattern)
+                    v.push_back(s);
             }
             ASSERT_EQ(v, res[j]);
         }
