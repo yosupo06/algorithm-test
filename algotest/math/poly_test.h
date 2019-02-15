037d1be32f1637cb1503ca8478e6a33a68ece76d
Kohei Morita <yosupo06@gmail.com>
2019-02-16 00:20:54 +0900

    add mul test

--- algotest/math/poly_test.h
+++ algotest/math/poly_test.h
@@ -97,6 +97,36 @@ TYPED_TEST_P(PolyTest, SubStressTest) {
     }
 }
 
-REGISTER_TYPED_TEST_CASE_P(PolyTest, AddStressTest, SubStressTest);
+TYPED_TEST_P(PolyTest, MulStressTest) {
+    using ll = long long;
+    using V = std::vector<ll>;
+    static const int N = 30;
+    constexpr ll kMod = PolyTesterBase::kMod;
+    algotest::random::Random gen;
+
+    for (int a_sz = 1; a_sz < N; a_sz++) {
+        for (int b_sz = 1; b_sz < N; b_sz++) {
+            TypeParam your_poly;
+            V a(a_sz), b(b_sz);
+            for (int i = 0; i < a_sz; i++)
+                a[i] = gen.uniform(1LL, kMod - 1);
+            for (int i = 0; i < b_sz; i++)
+                b[i] = gen.uniform(1LL, kMod - 1);
+            V ans(a_sz + b_sz - 1);
+            for (int i = 0; i < a_sz; i++) {
+                for (int j = 0; j < b_sz; j++) {
+                    ans[i + j] += a[i] * b[j] % kMod;
+                    ans[i + j] %= kMod;
+                }
+            }
+            while (ans.back() == 0)
+                ans.pop_back();
+            auto out = your_poly.mul(a, b);
+            ASSERT_EQ(ans, out);
+        }
+    }
+}
+
+REGISTER_TYPED_TEST_CASE_P(PolyTest, AddStressTest, SubStressTest, MulStressTest);
 
 }  // namespace algotest
